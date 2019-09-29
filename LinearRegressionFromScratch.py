64025e9b384458516468a840139f8c95d4c447c4
Arjun <arjunsekar1991@gmail.com>
2019-09-28 20:12:11 -0400

    adding class variables and costfunction

--- LinearRegressionFromScratch.py
+++ LinearRegressionFromScratch.py
@@ -1,2 +1,18 @@
+import numpy
+inputTestData = numpy.array([[1],[2]])
+actualValuesofY = numpy.array([[1],[2]])
+class LinearRegressionScratch:
+    def __init__(self, inputDataFrame,actualY):
+        self.inputData = inputDataFrame
+        self.actualValuesOfY = actualY
+        self.numberOfInstances, self.numberOfFeatures = self.inputData.shape
+        self.learningRate =0.01
+        self.MAX_ITER = 1000
 
+        # print(self.numberOfInstances)
+        # print(self.numberOfFeatures)
+    def  calculateCostJTheta(self,theta):
 
+        predictions = self.inputData.dot(theta)
+        cost = (1/2*self.numberOfInstances) * numpy.sum(numpy.square(predictions-self.actualValuesOfY))
+        return cost
